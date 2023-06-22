2e7e7d3cbc4d0931259aead1b6fad6db6693999a
dennisoshak <dennisoshak@gmail.com>
2023-06-22 14:46:25 +0300

    Procfile

--- frontend/src/services/authServer.js
+++ frontend/src/services/authServer.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.REACT_APP_BASEURL;
+const node_env = process.env.NODE_ENV;
 console.log(baseURL)
 export const sendSignUp = async (name, email, password) => {
   try {
